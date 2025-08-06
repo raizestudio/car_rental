988467b7ec56defd557ee8755b1b91651a8c469e
Joel <raizetvision@gmail.com>
2025-08-06 12:39:04 +0200

    include shims for vercel deploy

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,6 @@
-export {}
+import { ComponentCustomProperties } from 'vue'
 
-declare module 'vue' {
+declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     $capitalize: (word: string) => string
     $capitalizeWords: (str: string) => string
